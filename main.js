29d7c09bbb54398f736a03660a175c1f4baa13f5
Kori <furrycality@gmail.com>
2024-04-03 20:34:58 -0500

    Added .env support / Intents and partials added

--- main.js
+++ main.js
@@ -1,46 +1,49 @@
 "use strict";
 
-import * as Discord from "discord.js";
-import {Bot} from "../auth/SussE6.js";
+import { Client, GatewayIntentBits, Partials } from 'discord.js';
 import * as Persistence from "./src/persistence/dataManager.js";
 
 import * as Commands from "./commands/.commands.js";
 import * as Buttons from "./buttons/.buttons.js";
 import * as Modals from "./modals/.modals.js";
 
-const client = new Discord.Client({intents: [
-	Discord.IntentsBitField.Flags.Guilds,
-	Discord.IntentsBitField.Flags.GuildMessages,
-	Discord.IntentsBitField.Flags.DirectMessages,
-	Discord.IntentsBitField.Flags.GuildWebhooks,
-	Discord.IntentsBitField.Flags.MessageContent
-]});
+import { config } from 'dotenv';
+config();
 
-// process.exit(0);
+const client = new Client({
+    intents: Object.values(GatewayIntentBits),
+    partials: Object.values(Partials).map((partial) => partial),
+    allowedMentions: {
+        repliedUser: true,
+    },
+    restRequestTimeout: 20000,
+    ws: { properties: { browser: "Discord iOS" } }
+});
 
 client.on("interactionCreate", async (interaction) => {
-	if (interaction.isChatInputCommand())
-		return handleChatInputCommand(interaction);
-	if (interaction.isButton())
-		return handleButton(interaction);
-	if (interaction.isModalSubmit())
-		return handleModal(interaction);
+    if (interaction.isChatInputCommand())
+        return handleChatInputCommand(interaction);
+    if (interaction.isButton())
+        return handleButton(interaction);
+    if (interaction.isModalSubmit())
+        return handleModal(interaction);
 });
+
 async function handleChatInputCommand(command) {
-	if (!Persistence.canUse(command.user))
-		return Commands.ChatInput.verify.run(command);
-	return Commands.ChatInput[command.commandName].run(command);
+    if (!Persistence.canUse(command.user))
+        return Commands.ChatInput.verify.run(command);
+    return Commands.ChatInput[command.commandName].run(command);
 }
 async function handleButton(button) {
-	return Buttons[button.customId].run(button);
+    return Buttons[button.customId].run(button);
 }
 async function handleModal(modal) {
-	return Modals[modal.customId].run(modal);
+    return Modals[modal.customId].run(modal);
 }
-
 
 
 client.on("ready", () => {
-	console.log("ready...");
+    console.log("Ready...");
 });
-client.login(Bot.token);
+
+client.login(process.env['TOKEN_DISCORD'])
