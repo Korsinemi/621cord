c7f5337d74c0969eedaf856bd586de8c19ad4576
Kori <furrycality@gmail.com>
2024-04-04 21:31:41 -0500

    Small Fixed / Improved Request using Axios

--- src/e6Wrapper.js
+++ src/e6Wrapper.js
@@ -1,48 +1,55 @@
 "use strict";
 
-import fetch from "node-fetch";
-import {Tags} from "./tagManager.js";
-import {E6UserAgent} from "../../tokens/SussE6.js";
-import {Mutex} from "async-mutex";
-/*
-var ret = await fetch(`${url}`, {
-  method: "GET",
-  headers: {
-    "User-Agent": "SussE6 Discord Bot (by arden_arteles on e621)"
-  }
-});*/
+import axios from "axios";
+import { Tags } from "./tagManager.js";
 
-var rateLimit = new Mutex();
+const rateLimitDelay = 500; // Delay in milliseconds
 
-// doesn't use await but always returns a promise
 async function get(url) {
-    if (rateLimit.isLocked())
-      console.log("Rate limit locked, waiting to complete request...");
-    return rateLimit.acquire().then(release => {
-
-        let tempValue = fetch(url, {method: "GET", headers: {"User-Agent": E6UserAgent}});
-
-        return tempValue.then(ret => {
-            setTimeout(() => release(), 500);
-            return ret;
+    try {
+        const response = await axios.get(url, {
+            headers: { "User-Agent": process.env['E6UserAgent'] }
         });
-    });
+        await new Promise(resolve => setTimeout(resolve, rateLimitDelay));
+        return response;
+    } catch (error) {
+        console.error('Error:', error);
+        return null;
+    }
 }
 
 export async function getRandomPost(tags) {
-    if (!(tags instanceof Tags))
-      tags = new Tags(tags);
-    var a = get(`https://e621.net/posts/random.json?tags=${tags.string}`)
-        .then(res => res.json());
-    return a.then(data => new Post(data.post, `https://e621.net/posts/${data.post.id}.json?tags=${tags.string}`, "/posts/random"));
+    if (!(tags instanceof Tags)) {
+        tags = new Tags(tags);
+    }
+
+    try {
+        const response = await get(`https://e621.net/posts/random.json?tags=${tags.string}`);
+
+        if (!response || response.status !== 200) {
+            throw new Error('No response from the server or the status is not OK');
+        }
+
+        const data = response.data;
+
+        if (data.success) {
+            throw new Error('The API returned an error');
+        }
+
+        return new Post(data.post, `https://e621.net/posts/${data.post.id}.json?tags=${tags.string}`, "/posts/random");
+    } catch (error) {
+        console.error('Error:', error);
+        return false;
+    }
 }
 
 export class Post {
     constructor(post, url, methodCall) {
         Object.assign(this, post);
-        this.url = url.replace(".json","");
+        this.url = url.replace(".json", "");
         this.methodCall = methodCall;
     }
+
     get allTags() {
         return [...Object.values(this.tags)].flatMap(tag => tag).sort();
     }
--- src/replyBuilder.js
+++ src/replyBuilder.js
@@ -1,23 +1,82 @@
 "use strict";
 
-import {EmbedBuilder, ActionRowBuilder, ButtonBuilder} from "discord.js";
+import { EmbedBuilder, ActionRowBuilder, ButtonBuilder } from "discord.js";
 import * as Buttons from "../buttons/.buttons.js"
 
 export function url(post) {
-	return post.url;
+    return post.url;
 }
 
+console.log
+
 export function embed(post) {
-	var embed = new EmbedBuilder();
-	var actionRow = new ActionRowBuilder();
-	embed = embed.setDescription(post.allTags.join(", ").replaceAll("_", " "))
-		.setTitle(`#${post.id}`)
-		.setImage(post.sample.url)
-		.setURL(post.url);
-	actionRow.addComponents([
-		Buttons.new_random_post.component,
-		Buttons.delete_message.component,
-		Buttons.tags.component
-	]);
-	return {embeds: [embed], components: [actionRow]};
+    if (post === false) {
+        return { content: "I didn't find anything with those search terms." };
+    }
+
+    var embed = new EmbedBuilder();
+    var actionRow = new ActionRowBuilder();
+
+    var rating;
+    switch (post.rating) {
+        case "s":
+            rating = "SAFE 😺";
+            break;
+        case "e":
+            rating = "EXPLICIT ❤️";
+            break;
+        case "q":
+            rating = "QUESTIONABLE 🔞";
+            break;
+        default:
+            rating = "UNKNOWN";
+    }
+
+    var description
+    if (post.description === "") {
+        description = "No description"
+    } else {
+        description = post.description
+    }
+
+    embed = embed.setDescription(`
+        **Artist**: ${post.tags.artist.join(", ").replaceAll("_", " ")}
+        ------------
+        **Rating**: ${rating}
+        **Scores:** ✅ Up: **${post.score.up}** 
+                    ❎ Down: **${post.score.down}** 
+                    🦊 Total: **${post.score.total}**
+        ------------
+        **Description:** 
+        \`\`\`${description}\`\`\`
+        ------------
+        **Tags**
+        \`\`\`${post.allTags.join(", ").replaceAll("_", " ")}\`\`\`
+        `)
+        .setTitle(`Scrapped from e621 > #${post.id}`)
+        .setURL(post.url)
+        .setFooter({ text: `⭐ Favorited ${post.fav_count} times | 621cord by Korsinemi (reubuilded from susse6)`, iconURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQPDWhYX8_2HxocQsPiQkT7M5wwCWOCAqmkciBsyhkgA&s" });
+
+    var videoUrl;
+    if (["webm", "mp4"].includes(post.file.ext)) {
+        videoUrl = post.file.url;
+    } else if (["swf"].includes(post.file.ext)) {
+        embed.setImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9s92w2c8f5hNU3zEVxCv9dGrYLeh2qTeZ-tPt29wWXw&s")
+    } else {
+        embed.setImage(post.sample.url);
+    }
+
+    actionRow.addComponents([
+        Buttons.new_random_post.component,
+        Buttons.delete_message.component,
+        Buttons.tags.component
+    ]);
+
+    var response = { embeds: [embed], components: [actionRow], files: [] };
+    if (videoUrl) {
+        response = { embeds: [embed], components: [actionRow], files: [videoUrl] };
+    }
+    return response;
+
 }
+
